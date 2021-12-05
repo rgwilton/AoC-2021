455e2d117582dcf55860c858c227526b7805919a
Robert Wilton <robert.public@wilton.org.uk>
2021-12-05 14:17:06 +0000

    Slight cleanup for Ex5

--- src/main/scala/Exercises/Ex5.scala
+++ src/main/scala/Exercises/Ex5.scala
@@ -1,33 +1,25 @@
 package aoc
 
-import scala.collection.mutable
-
 object Ex5 extends Exercise:
   type ParsedInput = Seq[Line]
 
   case class Line(x1: Int, y1: Int, x2: Int, y2: Int):
-    def isHorizontal = y1 == y2
-    def isVertical = x1 == x2
-    def isDiagonal = !(isHorizontal || isVertical)
-    def minX = x1 min x2
-    def minY = y1 min y2
-    def maxX = x1 max x2
-    def maxY = y1 max y2
+    inline def isHorizontal = y1 == y2
+    inline def isVertical = x1 == x2
+    inline def isDiagonal = !(isHorizontal || isVertical)
 
   case class Map(xMax: Int, yMax: Int):
     val map = Array.ofDim[Int](xMax, yMax)
 
     inline def range(m: Int, n: Int) = 
-      if m < n then m to n
-      else if m > n then m to (n, step = -1)
-      else LazyList.continually(m)
+      if m < n then m to n else m to (n, step = -1)
 
-    def addLine(l: Line) =
-      if l.y1 == l.y2 then
-        for x <- l.minX to l.maxX do
+    def drawLine(l: Line) =
+      if l.isHorizontal then
+        for x <- range(l.x1, l.x2) do
           map(x)(l.y1) += 1
-      else if l.x1 == l.x2 then
-        for y <- l.minY to l.maxY do
+      else if l.isVertical then
+        for y <- range(l.y1, l.y2) do
           map(l.x1)(y) += 1
       else
         val xRange = range(l.x1, l.x2)
@@ -53,12 +45,10 @@ object Ex5 extends Exercise:
 
   def part1(input: ParsedInput) = 
     val m = Map(1000, 1000)
-    for l <- input.filterNot(_.isDiagonal) do
-      m.addLine(l)
+    input.filterNot(_.isDiagonal).foreach(m.drawLine)
     m.count
 
   def part2(input: ParsedInput) =
     val m = Map(1000, 1000)
-    for l <- input do
-      m.addLine(l)
+    input.foreach(m.drawLine)
     m.count
