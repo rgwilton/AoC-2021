6f26c701a086bf3e3dd9096f588691d22e857e08
Robert Wilton <robert.public@wilton.org.uk>
2021-12-06 08:22:04 +0000

    Day 6 Soln

---
+++ src/test/scala/Ex6Test.scala
@@ -0,0 +1,8 @@
+package aoc
+
+class Ex6Test extends ExTest(Ex6):
+  val testcases = Seq(
+    TestCase(
+   """3,4,3,1,2""", 5934L, 26984457539L),
+    TestCase(exInput, 379414L, 1705008653296L)
+  )
