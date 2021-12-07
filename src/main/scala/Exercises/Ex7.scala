bdb950867a3c498b54868c56bfd697eca6c89338
Robert Wilton <robert.public@wilton.org.uk>
2021-12-07 10:36:30 +0000

    Exercise 7 part 1 and 2

---
+++ src/main/scala/Exercises/Ex7.scala
@@ -0,0 +1,36 @@
+package aoc
+
+import scala.collection.mutable
+
+object Ex7 extends Exercise:
+  type ParsedInput = Seq[Int]
+
+  def parseInput(input: Iterator[String]) = input.next.asIntegers.toSeq
+
+  extension (xs: Seq[Int])
+    def mean = xs.sum/xs.length
+    def median = 
+      val sortedXs = xs.sorted
+      sortedXs(xs.size / 2)
+
+  def minFuel(input: ParsedInput, candidate: Int, fuelCalc: Int => Int) =
+    inline def decreasing(xs: LazyList[Int]) = xs.head > xs.last
+
+    // Select the last decrease value from increase/decreasing candidates.
+    inline def candidates(step: Int = 1) = 
+      LazyList.from(candidate, step).map(fuelCalc).sliding(2).dropWhile(decreasing(_)).next.head
+    inline def increasingCandidates = candidates(1)
+    inline def decreasingCandidates = candidates(-1)
+    increasingCandidates min decreasingCandidates
+
+  def part1(input: ParsedInput) = 
+    val candidate = input.median
+    def fuelCalc(target: Int) = input.foldRight(0)((x, sum) => sum + Math.abs(target - x))
+    minFuel(input, candidate, fuelCalc)
+
+  def part2(input: ParsedInput) =
+    val candidate = input.median
+    inline def fuelCost(distance: Int) = ((1 + distance) * distance) / 2
+    def fuelCalc(target: Int) = input.foldRight(0)((x, sum) => sum + fuelCost(Math.abs(target - x)))
+    minFuel(input, candidate, fuelCalc)
+
---
+++ src/test/scala/Ex7Test.scala
@@ -0,0 +1,8 @@
+package aoc
+
+class Ex7Test extends ExTest(Ex7):
+  val testcases = Seq(
+    TestCase(
+   "16,1,2,0,4,2,7,1,2,14", 37, 168),
+    TestCase(exInput, 331067, 92881128)
+  )
